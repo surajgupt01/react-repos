54fb19ef695dd625343b7831dc06f59eda9bfede
surajgupt01 <94984348+surajgupt01@users.noreply.github.com>
2024-08-13 21:55:00 +0530

    Update buttons.jsx

--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -5,7 +5,7 @@ const button=()=>{
 
     return(
         <div>
-            <a href="https://drive.google.com/file/d/1m91_wswmyJIupY6DOdQubCWC5Rsaf6rN/view?usp=sharing"><motion.button className="h-12 w-36 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 m-1" whileInView={{opacity:1}}
+            <a href="https://drive.google.com/file/d/1n6ljH8lKtzDFEM_CDMdS2mxBV_RBDptw/view?usp=sharing"><motion.button className="h-12 w-36 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 m-1" whileInView={{opacity:1}}
                         initial = {{opacity:0}}
                         transition={{duration:1}}>Resume</motion.button></a>
             <a href="#contact"><motion.button className="h-12 w-36 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 m-1"   whileInView={{opacity:1}}
